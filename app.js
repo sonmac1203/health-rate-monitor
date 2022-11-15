b60cbbcd61627a63a7455db1e28f07fa217bc6d9
Son Mac <mactranthienson@gmail.com>
2022-11-14 23:18:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const bodyParser = require('body-parser'); // Parses JSON in body
+
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
@@ -13,6 +15,30 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// This is to enable cross-origin access
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,14 +47,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
-app.get('/login', function (req, res) {
-  res.sendFile(path.join(__dirname, '/views/login.html'));
-});
-
-app.get('/signup', function (req, res) {
-  res.sendFile(path.join(__dirname, '/views/signup.html'));
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
