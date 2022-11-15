597acf7e4e3d56ff8a55fc3fe5c9580bff4db6d0
Son Mac <mactranthienson@gmail.com>
2022-11-14 23:19:06 -0700

    Update user schema for db

--- schemas/user.js
+++ schemas/user.js
@@ -3,6 +3,7 @@ const db = require('../db');
 const userSchema = new db.Schema({
   email: String,
   passwordHash: String,
+  lastAccess: { type: Date, default: Date.now },
 });
 
 const User = db.model('Users', userSchema);
