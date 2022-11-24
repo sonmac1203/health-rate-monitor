6bba60393984058adc18d02d0ef59a3a8497a6e6
Son Mac <mactranthienson@gmail.com>
2022-11-23 17:08:34 -0700

    Update user schema

--- schemas/user.js
+++ schemas/user.js
@@ -4,6 +4,15 @@ const userSchema = new db.Schema({
   email: String,
   passwordHash: String,
   lastAccess: { type: Date, default: Date.now },
+  devices: {
+    type: [
+      {
+        device_id: String,
+        time_added: { type: Date, default: Date.now },
+      },
+    ],
+    default: [],
+  },
 });
 
 const User = db.model('Users', userSchema);
