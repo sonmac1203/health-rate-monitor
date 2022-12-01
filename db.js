0f6bbe3ceace3ead95cee840b7e1521e578899b8
shellouff <shellouff@email.arizona.edu>
2022-11-30 20:49:43 -0700

    Revert "Update db.js"
    This reverts commit 4682bddce4b63057ba6d2005d5f6ec6062ab9aaa.

--- db.js
+++ db.js
@@ -1,18 +1,13 @@
-//cloud.mongodb.com info:
-//Cluster username and Password:
-//  username: heartrate
-//  passwoord: webdev413
 const mongoose = require('mongoose');
 
 // const username = process.env.MONGO_USERNAME;
-const username = 'heartrate';
+const username = 'sontmac1203';
 // const password = process.env.MONGO_PASSWORD;
-const password = 'webdev413';
-const databaseName = 'HeartMonitorDB';
+const password = 'thienson123';
+const databaseName = 'ece-413-final-project';
 
 try {
   mongoose.connect(
-    // `mongodb+srv://${username}:${password}@heartmonitordb.0ivesko.mongodb.net/?retryWrites=true&w=majority`
     `mongodb+srv://${username}:${password}@cluster0.l515c.mongodb.net/${databaseName}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
