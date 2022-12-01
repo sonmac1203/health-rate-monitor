4682bddce4b63057ba6d2005d5f6ec6062ab9aaa
shellouff <shellouff@email.arizona.edu>
2022-11-30 19:55:47 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
+//cloud.mongodb.com info:
+//Cluster username and Password:
+//  username: heartrate
+//  passwoord: webdev413
 const mongoose = require('mongoose');
 
 // const username = process.env.MONGO_USERNAME;
-const username = 'sontmac1203';
+const username = 'heartrate';
 // const password = process.env.MONGO_PASSWORD;
-const password = 'thienson123';
-const databaseName = 'ece-413-final-project';
+const password = 'webdev413';
+const databaseName = 'HeartMonitorDB';
 
 try {
   mongoose.connect(
+    // `mongodb+srv://${username}:${password}@heartmonitordb.0ivesko.mongodb.net/?retryWrites=true&w=majority`
     `mongodb+srv://${username}:${password}@cluster0.l515c.mongodb.net/${databaseName}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
